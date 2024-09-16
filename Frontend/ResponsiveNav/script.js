2c6665e7b147c4c65cc53d159cd79f0486c5cdb2
Runkana Durga Prasad <runkanaprasad1@gmail.com>
2024-09-16 18:51:00 +0530

    Add files via upload

---
+++ Frontend/ResponsiveNav/script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  const navLinks = document.getElementById("mob-view");
+  if (navLinks.style.display === "flex") {
+    navLinks.style.display = "none";
+  } else {
+    navLinks.style.display = "flex";
+  }
+}
